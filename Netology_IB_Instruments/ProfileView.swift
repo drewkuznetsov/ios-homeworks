a6a765c7f29b0a771ae2b53a13e7a22c3ad632f6
drewkuznetsov <8150309@gmail.com>
2022-03-02 16:38:52 +0300

    ProfileView xib & swift files added. IBOutlets edded

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Андрей Кузнецов on 02.03.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    @IBOutlet weak var nameLable: UILabel!
+        
+    @IBOutlet weak var dateLable: UILabel!
+    
+    @IBOutlet weak var cityLable: UILabel!
+    
+    @IBOutlet weak var infoTextView: UITextView!
+    
+}
