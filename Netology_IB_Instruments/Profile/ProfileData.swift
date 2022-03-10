a79d7f70ef77b317dcd32c1f88e1168b61550498
drewkuznetsov <8150309@gmail.com>
2022-03-10 15:37:46 +0300

    ProfileHeaderView added

---
+++ Netology_IB_Instruments/Profile/ProfileData.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileData.swift
+//  Netology_IB_Instruments
+//
+//  Created by Андрей Кузнецов on 10.03.2022.
+//
+
+import Foundation
+
+struct ProfileData {
+    let name: String
+    let image: String
+    var status: String
+}
--- Netology_IB_Instruments/Profile/ProfileViewController.swift
+++ Netology_IB_Instruments/Profile/ProfileViewController.swift
@@ -10,7 +10,39 @@ import UIKit
 class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        navigationItem.title = "Profile Page"
+        navigationController?.navigationBar.prefersLargeTitles = false
+        
+        view.backgroundColor = .lightGray
+        
+    }
+    
+    override func viewWillLayoutSubviews() {
+
+        let profileHeader = ProfileHeaderView(frame: view.bounds)
+
+        view.addSubview(profileHeader)
+
+        profileHeader.translatesAutoresizingMaskIntoConstraints = false
+
+        profileHeader.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        profileHeader.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        
+        if let bottomAnchor = navigationController?.navigationBar.bottomAnchor {
+            profileHeader.topAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        } else {
+            profileHeader.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        }
+        
+        if let topAnchor = tabBarController?.tabBar.topAnchor {
+            profileHeader.bottomAnchor.constraint(equalTo: topAnchor).isActive = true
+        } else {
+            profileHeader.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        }
+
     }
 
 }
