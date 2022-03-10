48c380daad785afc7a7028dc625f3591b73567f7
drewkuznetsov <8150309@gmail.com>
2022-03-10 15:30:02 +0300

    StoryBoard deleted. Profile group added

---
+++ Netology_IB_Instruments/FeedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FeedViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Андрей Кузнецов on 10.03.2022.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.title = "Feed Page"
+    }
+
+
+}
---
+++ Netology_IB_Instruments/Profile/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Андрей Кузнецов on 10.03.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
